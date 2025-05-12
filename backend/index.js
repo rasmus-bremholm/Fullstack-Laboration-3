12f40718d959ac05aafdb1c113a9f71e63f58d6d
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-05-12 12:33:45 +0200

    Test run

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Imports
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const pg_1 = require("pg");
+// Global Variables
+const port = 1337;
+// Initialisation
+const app = (0, express_1.default)();
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+dotenv_1.default.config();
+const client = new pg_1.Client({
+    connectionString: process.env.PGURI,
+});
+client.connect();
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:" + port);
+});
---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+// Imports
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { Client } from "pg";
+
+// Global Variables
+const port = 1337;
+
+// Initialisation
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+const client = new Client({
+	connectionString: process.env.PGURI,
+});
+client.connect();
+
+app.listen(port, () => {
+	console.log("Server is running on http://localhost:" + port);
+});
