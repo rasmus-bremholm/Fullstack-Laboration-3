9d0f7c118828f8d96f9bd0174b482bc8c432bbfa
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-05-13 09:30:08 +0200

    Update form.tsx

--- frontend/app/actions/form.tsx
+++ frontend/app/actions/form.tsx
@@ -1,1 +1,7 @@
-"use server";
+// Intressant som fan https://nextjs.org/docs/app/getting-started/updating-data
+// https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData
+
+export async function createPost(formData: FormData) {
+	"use server";
+	const title = formData.get("title");
+}
