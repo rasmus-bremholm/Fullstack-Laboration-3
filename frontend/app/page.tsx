2dc31d20f00bd1883a5e145344fa5d82b2d0c5de
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-05-15 14:08:40 +0200

    Really vercel?

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import Test from "./components/test-component";
-import styles from "./page.module.css";
+//import styles from "./page.module.css";
 
 export default function Home() {
 	return <Test />;
