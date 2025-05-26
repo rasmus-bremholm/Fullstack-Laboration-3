d60763ef8e1370ee624ac60be94259464fc16a12
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-05-26 14:35:14 +0200

    Update modal forms

--- frontend/app/components/Modals.tsx
+++ frontend/app/components/Modals.tsx
@@ -1,19 +1,55 @@
 import styles from "../styles/modal.module.css";
+import { useState, useEffect, useContext } from "react";
+import type { User } from "../types/types";
 
-export function EditModal({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+interface EditProps {
+	isOpen: boolean;
+	onClose: () => void;
+}
+
+export function EditModal({ isOpen, onClose }: EditProps) {
 	if (!isOpen) return null;
 
 	return (
 		<div className={styles.backdrop} onClick={onClose}>
 			<div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-				<h2>Edit Student</h2>
+				<h2>Edit Avatar</h2>
 				<form action=''></form>
 			</div>
 		</div>
 	);
 }
 
-export function DeleteModal({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+export function EditStudentModal({ isOpen, onClose }: EditProps) {
+	const [studentInfo, setStudentInfo] = useState<User>({
+		id: 0,
+		first_name: "",
+		last_name: "",
+		email: "",
+		password: "",
+		profile_picture: null,
+	});
+
+	useEffect(() => {
+		//
+	}, []);
+
+	const handleSubmit = () => {
+		const token = localStorage.getItem("token");
+	};
+
+	if (!isOpen) return null;
+	return (
+		<div className={styles.backdrop} onClick={onClose}>
+			<div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+				<h2>Edit Student</h2>
+				<form onSubmit={handleSubmit}></form>
+			</div>
+		</div>
+	);
+}
+
+export function DeleteModal({ isOpen, onClose }: EditProps) {
 	if (!isOpen) return null;
 	return (
 		<div className={styles.backdrop} onClick={onClose}>
